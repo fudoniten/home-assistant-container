96460ea9591504bbe5a3aec0c6401e2f7cea3926
niten <niten@fudo.org>
2024-08-01 16:36:20 -0700

    Oh, and actually use the new version

--- extended-openai-conversation.nix
+++ extended-openai-conversation.nix
@@ -1,10 +1,10 @@
 { extended-openai-conversation, lib, buildHomeAssistantComponent
-, python312Packages, version, ... }:
+, python312Packages, version, openai, ... }:
 
 buildHomeAssistantComponent {
   src = extended-openai-conversation;
   owner = "jekalmin";
   domain = "extended_openai_conversation";
   version = version;
-  propagatedBuildInputs = with python312Packages; [ openai ];
+  propagatedBuildInputs = [ openai ];
 }
