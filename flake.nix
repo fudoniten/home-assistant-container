699020d4250b144100dfd3408d28258bb867c602
niten <niten@fudo.org>
2024-08-02 11:19:25 -0700

    Will pin pkgsUnstable

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
     arion.url = "github:hercules-ci/arion";
     extended-openai-conversation = {
@@ -11,15 +12,17 @@
     };
   };
 
-  outputs =
-    { self, nixpkgs, utils, arion, extended-openai-conversation, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgsUnstable, utils, arion
+    , extended-openai-conversation, ... }@inputs:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages."${system}";
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        pkgsUnstable = nixpkgsUnstable.legacyPackages."${system}";
       in {
         packages = rec {
           extended_openai_conversation =
             pkgs.callPackage ./extended-openai-conversation.nix {
-              inherit extended-openai-conversation;
+              inherit extended-openai-conversation pkgsUnstable;
               version = "1.0.3";
             };
         };
