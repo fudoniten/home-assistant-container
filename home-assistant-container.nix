f7418784da79c6b4741f063ca18cd26b4ea99900
niten <niten@fudo.org>
2024-08-03 10:18:28 -0700

    Try enabling components in the cconfig

--- home-assistant-container.nix
+++ home-assistant-container.nix
@@ -213,6 +213,9 @@ in {
                     # };
                     mobile_app = { };
                     cloud = { };
+                    history = { };
+                    energy = { };
+                    recorder = { };
                     http = {
                       server_host = [ "0.0.0.0" ];
                       server_port = 8123;
