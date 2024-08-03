6c97a5a65c0d51b0f20eee1c8786ca6875d890db
niten <niten@fudo.org>
2024-08-02 18:44:45 -0700

    Hardcode the correct version of openai

--- extended-openai-conversation.nix
+++ extended-openai-conversation.nix
@@ -1,10 +1,20 @@
-{ extended-openai-conversation, lib, buildHomeAssistantComponent
-, python312Packages, version, ... }:
+{ extended-openai-conversation, lib, fetchFromGithub
+, buildHomeAssistantComponent, python312Packages, version, ... }:
 
 buildHomeAssistantComponent {
   src = extended-openai-conversation;
   owner = "jekalmin";
   domain = "extended_openai_conversation";
   version = version;
-  propagatedBuildInputs = [ python312Packages.openai ];
+  propagatedBuildInputs = let
+    openai = python312Packages.openai.overrideAttrs (oldAttrs: rec {
+      version = "1.3.8";
+      src = fetchFromGithub {
+        owner = "openai";
+        repo = "openai-python";
+        rev = "ref/tags/v${version}";
+        hash = "sha256-yU0XWEDYl/oBPpYNFg256H0Hn5AaJiP0vOQhbRLnAxQ=";
+      };
+    });
+  in [ openai ];
 }
