8f5093d1667d4ce5cdeff42c30ce0512833b257b
niten <niten@fudo.org>
2025-06-25 12:31:31 -0700

    Change approach...just use py313 openai instead?

--- extended-openai-conversation.nix
+++ extended-openai-conversation.nix
@@ -1,21 +1,20 @@
 { extended-openai-conversation, lib, fetchFromGitHub
-, buildHomeAssistantComponent, python3, python3Packages, version, ... }:
+, buildHomeAssistantComponent, python313Packages, version, ... }:
 
 let
-  openai = python3.pkgs.buildPythonPackage rec {
-    pname = "openai";
+  openai = python313Packages.openai.overrideAttrs (oldAttrs: rec {
     version = "1.13.3";
-    format = "setuptools";
-
     src = fetchFromGitHub {
       owner = "openai";
       repo = "openai-python";
       rev = "refs/tags/v${version}";
       hash = "sha256-8SHXUrPLZ7lgvB0jqZlcvKq5Zv2d2UqXjJpgiBpR8P8=";
     };
-
-    doCheck = false;
-  };
+    disabledTests = oldAttrs.disabledTests ++ [
+      "test_retrying_timeout_errors_doesnt_leak"
+      "test_retrying_status_errors_doesnt_leak"
+    ];
+  });
 
 in buildHomeAssistantComponent {
   src = extended-openai-conversation;
