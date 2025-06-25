0554d763a5f3ee956526baf5daa5f8874aba4641
niten <niten@fudo.org>
2025-06-25 10:57:58 -0700

    Explicitly build with the correct py ver

--- extended-openai-conversation.nix
+++ extended-openai-conversation.nix
@@ -1,27 +1,27 @@
 { extended-openai-conversation, lib, fetchFromGitHub
 , buildHomeAssistantComponent, python3, python3Packages, version, ... }:
 
-buildHomeAssistantComponent {
+let
+  py = buildHomeAssistantComponent.python;
+  openai = py.pkgs.buildPythonPackage rec {
+    pname = "openai";
+    version = "1.13.3";
+    format = "setuptools";
+
+    src = fetchFromGitHub {
+      owner = "openai";
+      repo = "openai-python";
+      rev = "refs/tags/v${version}";
+      hash = "sha256-8SHXUrPLZ7lgvB0jqZlcvKq5Zv2d2UqXjJpgiBpR8P8=";
+    };
+
+    doCheck = false;
+  };
+
+in buildHomeAssistantComponent {
   src = extended-openai-conversation;
   owner = "jekalmin";
   domain = "extended_openai_conversation";
   version = version;
-  propagatedBuildInputs = let
-    openai = (python3Packages.override {
-      inherit (buildHomeAssistantComponent) python;
-    }).openai.overrideAttrs (oldAttrs: rec {
-      version = "1.13.3";
-      src = fetchFromGitHub {
-        owner = "openai";
-        repo = "openai-python";
-        rev = "refs/tags/v${version}";
-        hash = "sha256-8SHXUrPLZ7lgvB0jqZlcvKq5Zv2d2UqXjJpgiBpR8P8=";
-      };
-      propagatedBuildInputs = [ buildHomeAssistantComponent.python ];
-      disabledTests = oldAttrs.disabledTests ++ [
-        "test_retrying_timeout_errors_doesnt_leak"
-        "test_retrying_status_errors_doesnt_leak"
-      ];
-    });
-  in [ openai ];
+  propagatedBuildInputs = [ openai ];
 }
