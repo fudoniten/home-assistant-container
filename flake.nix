d767b67984c0b3762f9315c26c35301fd8ab6494
niten <niten@fudo.org>
2025-09-06 14:47:41 -0700

    Switch to unstable channel for home-assistant

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Assistant running in a container";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
     arion.url = "github:hercules-ci/arion";
     hass-node-red = {
